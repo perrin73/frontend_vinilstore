aa4ed33019cad9d1a8bb97345fc32776a1a27cd2
perrin73 <cgaratea@gmail.com>
2023-06-27 00:06:14 -0400

    se agrega alerta de agregado al carro

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,63 +1,57 @@
-import React, { useEffect, useState,useContext } from "react";
-import contextVinil from "../contexto/ContextVinil";
+import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import contextVinil from "../contexto/ContextVinil";
 
 function Galeria() {
   const [publicaciones, setPublicaciones] = useState([]);
   const [filtroEstado, setFiltroEstado] = useState("");
   const [filtroNombre, setFiltroNombre] = useState("");
   const [publicacionesFiltradas, setPublicacionesFiltradas] = useState([]);
-  const { usuario, setUsuario,agregarAlCarrito,carrito } = useContext(contextVinil);
+  const { usuario, setUsuario, agregarAlCarrito, carrito } = useContext(
+    contextVinil
+  );
   const [conLogin, setConLogin] = useState(false);
+  const [agregado, setAgregado] = useState(false);
   const [estaCargando, setEstaCargando] = useState(false);
-  useEffect(() => {
 
-    if(Object.keys(usuario).length !== 0){setConLogin(true)}
+  useEffect(() => {
+    if (Object.keys(usuario).length !== 0) {
+      setConLogin(true);
+    }
     const fetchData = async () => {
-      setEstaCargando(true)
+      setEstaCargando(true);
       try {
         const response = await fetch(
           "https://backend-vinilstore.vercel.app/laspublicaciones",
           { method: "GET" }
         );
-        let data = await response.json();
-        
-        if(usuario.id){
-          data = data.filter((publi)=>
-              publi.id == usuario.id
-          )
-        }
+        const data = await response.json();
         setPublicaciones(data.data);
-        setEstaCargando(false)
+        setEstaCargando(false);
       } catch (error) {
         console.log("Error fetching data:", error);
-        setEstaCargando(false)
+        setEstaCargando(false);
       }
     };
-
     fetchData();
   }, []);
 
   useEffect(() => {
     const filtrarPublicaciones = () => {
       let publicacionesFiltradas = publicaciones;
-      
-      if (conLogin){
-      publicacionesFiltradas = publicacionesFiltradas.filter(
-        (publicacion) => publicacion.usuario_id !== usuario.datos.id
-      );
+
+      if (conLogin) {
+        publicacionesFiltradas = publicacionesFiltradas.filter(
+          (publicacion) => publicacion.usuario_id !== usuario.datos.id
+        );
       }
 
-      console.table(publicacionesFiltradas)
-
-      // Filtrar por estado
       if (filtroEstado !== "") {
         publicacionesFiltradas = publicacionesFiltradas.filter(
           (publicacion) => publicacion.estado === filtroEstado
         );
       }
 
-      // Filtrar por nombre o artista
       if (filtroNombre !== "") {
         const filtro = filtroNombre.toLowerCase();
         publicacionesFiltradas = publicacionesFiltradas.filter(
@@ -73,14 +67,21 @@ function Galeria() {
     filtrarPublicaciones();
   }, [filtroEstado, filtroNombre, publicaciones]);
 
-  console.table(carrito)
-  
+  const alCarrito = (publicacion) => {
+    agregarAlCarrito(publicacion);
+    setAgregado(true);
+    setTimeout(() => {
+      setAgregado(false);
+    }, 2000);
+  };
+
   return (
     <>
       <div className="row m-0">
         <div className="col-lg-3 col-md-3 col-sm-4">
-          
-          <div className="text-light mt-3 text-center mb-3 bg-dark rounded-5">filtro por estado :</div>
+          <div className="text-light mt-3 text-center mb-3 bg-dark rounded-5">
+            Filtro por estado:
+          </div>
           <select
             className="form-select mb-2"
             value={filtroEstado}
@@ -92,7 +93,9 @@ function Galeria() {
             <option value="Defecto estetico">Defecto estetico</option>
             <option value="Defecto audio">Defecto audio</option>
           </select>
-          <div className="text-light mt-3 text-center mb-3 bg-dark rounded-5">filtro por nombre o artista :</div>
+          <div className="text-light mt-3 text-center mb-3 bg-dark rounded-5">
+            Filtro por nombre o artista:
+          </div>
           <input
             type="text"
             className="form-control"
@@ -100,13 +103,25 @@ function Galeria() {
             value={filtroNombre}
             onChange={(e) => setFiltroNombre(e.target.value)}
           />
+          {agregado && (
+                        <div className="alert alert-success  position-absolute mt-5 ">
+                          ¡Artículo agregado al carrito! 👍
+                        </div>
+                      )}
         </div>
         <div className="col-lg-9 col-md-9 col-sm-12">
-        { estaCargando ? (
-          <div className="text-center mt-2">
-          <img className="rotarimage" style={{ maxHeight: "75%",maxWidth: "75%" }} src={'../src/assets/disco.png'} alt="cargando"></img>
-          </div>):('')
-          } 
+          {estaCargando ? (
+            <div className="text-center mt-2">
+              <img
+                className="rotarimage"
+                style={{ maxHeight: "75%", maxWidth: "75%" }}
+                src="../src/assets/disco.png"
+                alt="cargando"
+              />
+            </div>
+          ) : (
+            ""
+          )}
           <div className="row mt-3">
             {publicacionesFiltradas.map((publicacion) => (
               <div
@@ -129,7 +144,18 @@ function Galeria() {
                     </p>
                     <p className="card-text">{publicacion.estado}</p>
                   </div>
-                  {conLogin && (<button className="btn btn-sm btn-info mx-auto mb-1" style={{width:"40px"}} onClick={() => agregarAlCarrito(publicacion)}><i className="bi bi-cart"></i></button>)}
+                  {conLogin && (
+                    <div className="text-center">
+                      <button
+                        className="btn btn-sm btn-info mx-auto mb-1"
+                        style={{ width: "40px" }}
+                        onClick={() => alCarrito(publicacion)}
+                      >
+                        <i className="bi bi-cart"></i>
+                      </button>
+                      
+                    </div>
+                  )}
                 </div>
               </div>
             ))}
